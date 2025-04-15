22b0d26aad5167e1dd3fd28cdadc32272604174d
Gurjyot Anand <gurjyotanand@Gurjyots-MacBook-Air.local>
2025-04-15 16:39:07 -0400

    changes in main.tf

--- terraform-aws-ha-webapp/main.tf
+++ terraform-aws-ha-webapp/main.tf
@@ -16,19 +16,19 @@ resource "aws_vpc" "pj1_main" {
 }
 
 resource "aws_subnet" "public_1" {
-  vpc_id = aws_vpc.pf1_main.id
+  vpc_id = aws_vpc.pj1_main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"  
   }
 
 resource "aws_subnet" "public_2" {
-  vpc_id = aws_vpc.pf1_main.id
+  vpc_id = aws_vpc.pj1_main.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1b"
 }
 
-resource "aws_lb" "pf1_web_alb" {
-  name = "pf1_web-alb"
+resource "aws_lb" "pj1_web_alb" {
+  name = "pj1_web-alb"
   internal = false
   load_balancer_type = "application"
   subnets = [ aws_subnet.public_1.id, aws_subnet.public_2.id ]
