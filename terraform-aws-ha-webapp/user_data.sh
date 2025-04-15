d862659920c9acd127b6b64cf5442c55c1b99044
Gurjyot Anand <gurjyotanand@Gurjyots-MacBook-Air.local>
2025-04-15 16:29:39 -0400

    changes to link

--- terraform-aws-ha-webapp/user_data.sh
+++ terraform-aws-ha-webapp/user_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum update -y
 amazon-linux-extras install ansible2 -y
-git clone https://github.com/yourusername/ansible-config.git /tmp/ansible
+git clone git clone https://github.com/gurjyotanand/terraform_anisble_high_availability_server_project.git
+ /tmp/ansible
 ansible-playbook /tmp/ansible/webserver.yml
