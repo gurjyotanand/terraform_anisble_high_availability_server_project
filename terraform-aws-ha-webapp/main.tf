fb50499edf4355dd3d3f793eaf5f94c4ae27c9d9
Gurjyot Anand <gurjyotanand@Gurjyots-MacBook-Air.local>
2025-04-15 22:23:32 -0400

    IGW added in main.tf

--- terraform-aws-ha-webapp/main.tf
+++ terraform-aws-ha-webapp/main.tf
@@ -15,16 +15,25 @@ resource "aws_vpc" "pj1_main" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.pj1_main.id
+  tags = {
+    Name="pj1-igw"
+  }
+}
+
 resource "aws_subnet" "public_1" {
   vpc_id = aws_vpc.pj1_main.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"  
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
   }
 
 resource "aws_subnet" "public_2" {
   vpc_id = aws_vpc.pj1_main.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_lb" "pj1_web_alb" {
@@ -36,7 +45,7 @@ resource "aws_lb" "pj1_web_alb" {
 
 resource "aws_launch_template" "pj1_lt" {
   name_prefix = "pj1_server"
-  image_id = "ami-0c55b159cbfafe1f0" 
+  image_id = "ami-04985531f48a27ae7" 
   instance_type = "t2.micro"
   user_data = filebase64("user_data.sh")
 }
@@ -49,4 +58,25 @@ resource "aws_autoscaling_group" "pj1_asg" {
   launch_template {
     id = aws_launch_template.pj1_lt.id
   }
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.pj1_main.id
+  route {
+    cidr_block = "0.0.0.0/0"  # Route all traffic to IGW
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Associate public subnets with the route table
+resource "aws_route_table_association" "public_1_rt_assoc" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_2_rt_assoc" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public_rt.id
 }
